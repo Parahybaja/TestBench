39e9123437a79ac210377961c2ca096b4db61ce4
Gabriel D. Medeiros <143225924+gabrielmdrss@users.noreply.github.com>
2026-04-24 14:43:21 -0300

    chore: Remoção dos Testes do código exemplo

--- src/UserCode.cpp
+++ src/UserCode.cpp
@@ -3,13 +3,14 @@
 #include "LcdModule.h"
 
 /* USER CODE BEGIN INCLUDES */
+
 /* USER CODE END INCLUDES */
 
 /* USER CODE BEGIN VARIABLES */
-static int      contador        = 0;
-static int      contador1       = 0;
-static uint32_t lastDisplayUpdate = (uint32_t)(-1000UL); // força update imediato na 1ª chamada
+
 /* USER CODE END VARIABLES */
+
+static uint32_t lastDisplayUpdate = (uint32_t)(-1000UL); // força update imediato na 1ª chamada
 
 // =============================================================
 // UserCode_Setup
@@ -42,15 +43,6 @@ void UserCode_Loop() {
 void UserCode_UpdateDisplay(uint32_t elapsedMs) {
     /* USER CODE BEGIN UPDATE_DISPLAY */
 
-    if (elapsedMs - lastDisplayUpdate >= 1000) {
-        lastDisplayUpdate = elapsedMs;
-        contador++;
-        contador1 += 3;
-        char linha[17];
-        snprintf(linha, sizeof(linha), "C1: %d  C2: %d", contador, contador1);
-        LcdModule_ShowMessage(linha, "T: 24.5C  U: 60%");
-    }
-
     /* USER CODE END UPDATE_DISPLAY */
 }
 
@@ -62,7 +54,7 @@ void UserCode_UpdateDisplay(uint32_t elapsedMs) {
 // =============================================================
 const char* UserCode_GetCsvHeader() {
     /* USER CODE BEGIN CSV_HEADER */
-    return "time_ms,contador,contador1\n";
+    return "\n";
     /* USER CODE END CSV_HEADER */
 }
 
@@ -77,11 +69,7 @@ const char* UserCode_GetDataRow(uint32_t timestampMs) {
     static char buf[128];
 
     /* USER CODE BEGIN DATA_ROW */
-    snprintf(buf, sizeof(buf), "%lu,%d,%d\n",
-        (unsigned long)timestampMs,
-        contador,
-        contador1
-    );
+
     /* USER CODE END DATA_ROW */
 
     return buf;
@@ -93,8 +81,8 @@ const char* UserCode_GetDataRow(uint32_t timestampMs) {
 // =============================================================
 void UserCode_Stop() {
     /* USER CODE BEGIN STOP */
-    contador          = 0;
-    contador1         = 0;
-    lastDisplayUpdate = (uint32_t)(-1000UL); // força update imediato na próxima sessão
+
     /* USER CODE END STOP */
+    
+    lastDisplayUpdate = (uint32_t)(-1000UL); // força update imediato na próxima sessão
 }
